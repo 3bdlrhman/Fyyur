168fbcdaddeac6a301d6d0f112401cdd69a404a1
Abdlrhman Ahmed <abdlrhman.soudi@yahoo.com>
2021-01-24 22:22:17 +0200

    use JOIN at queries and Decoupling Models

--- starter_code/app.py
+++ starter_code/app.py
@@ -10,59 +10,20 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+# after seperating models
+from Models import db, Show, Artist, Venue
 
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 moment = Moment(app)
 app.config.from_object('config')
-
-#----------------------------------------------------------------------------#
-#                  Models
-#----------------------------------------------------------------------------#
-
-class Show(db.Model):
-      __tabblename__ = 'Show'
-      id = db.Column(db.Integer, primary_key=True)
-      venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-      artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-      start_time = db.Column(db.String, nullable=False)
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website = db.Column(db.String(120))
-    seeking_talent= db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(120))
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue= db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(120))
-    website = db.Column(db.String(120))
+db.init_app(app)
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -96,10 +57,9 @@ def venues():
   query = db.session.query(Venue.city.distinct().label("city"))
   cities = [row.city for row in query.all()]
   
-  # list contains final returned objects
+  # final returned objects
   objs=[]
   
-  # loop through all of these cities and find venues
   for city in cities:        
         venues_list = Venue.query.filter_by(city=city).all()
         ven_obj_list = []
@@ -173,15 +133,9 @@ def show_venue(venue_id):
   this_venue = Venue.query.get(venue_id)  
   shows = Show.query.filter(Show.venue_id== this_venue.id).all()
   
-  upcoming_shows=[]
-  past_shows=[]  
-  for show in shows:
-        t = str(show.start_time)
-        if datetime.strptime(t,'%Y-%m-%d %H:%M:%S') > datetime.now():
-              upcoming_shows.append(show)
-        else:
-              past_shows.append(show)
-  
+  upcoming_shows= db.session.query(Show).join(Venue).filter(Show.start_time > datetime.now()).filter(Show.venue_id==venue_id).all()
+  past_shows= db.session.query(Show).join(Venue).filter(Show.start_time < datetime.now()).filter(Show.venue_id==venue_id).all()
+
   upcoming_shows_lst=[]
   past_shows_lst=[]  
   for i in upcoming_shows:
@@ -249,7 +203,6 @@ def create_venue_submission():
     flash('Venue ' + request.form['name'] + ' was successfully listed!')
   except:
     db.session.rollback()
-    # flash('An error occurred. Venue ' + data.name + ' could not be listed.')
   finally:
     db.session.close()
     
@@ -259,8 +212,6 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
   try:
     record = Venue.query.get(venue_id)
     db.session.delete(record)
@@ -329,15 +280,8 @@ def show_artist(artist_id):
     
   shows = Show.query.filter(Show.artist_id== this_artist.id).all()
 
-  upcoming_shows=[]
-  past_shows=[]
-
-  for show in shows:
-        t = str(show.start_time)
-        if datetime.strptime(t,'%Y-%m-%d %H:%M:%S') < datetime.now():
-              upcoming_shows.append(show)
-        else:
-              past_shows.append(show)
+  upcoming_shows= db.session.query(Show).join(Artist).filter(Show.start_time > datetime.now()).filter(Artist.id==artist_id).all()
+  past_shows= db.session.query(Show).join(Artist).filter(Show.start_time < datetime.now()).filter(Artist.id==artist_id).all()
 
   upcoming_shows_lst=[]
   past_shows_lst=[]
@@ -390,46 +334,18 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
   # TODO: populate form with fields from artist with ID <artist_id>
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
   # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
-
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
+
   # TODO: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
@@ -475,8 +391,6 @@ def create_artist_submission():
 
 @app.route('/shows')
 def shows():
-  # displays list of shows at /shows
-  # TODO: replace with real venues data. num_shows should be aggregated based on number of upcoming shows per venue.
   info=db.session.query(
     Venue.id, Venue.name, Artist.id, Artist.name, Artist.image_link, 
     Show.start_time).select_from(Show).join(Venue, Venue.id==Show.venue_id).join(Artist, Artist.id==Show.artist_id).all()
@@ -523,18 +437,18 @@ def create_show_submission():
   
   return render_template('pages/home.html')
 
-##############################################################
-##############################################################
+          ##############################################################
+          ##############################################################
 
-         #########      ####     ######  ##   ######## 
-         ##########    ##  ##    ##  ##  ##   ##          
-         ###    ####  ##    ##   ##  ##  ##   ##
-         ###    ####  ##    ##   ##  ##  ##   #######         
-         ##########    ##  ##    ##  ##  ##   ##   
-         #########      ####     ##  ######   ########      
+                   #########      ####     ######  ##   ######## 
+                   ##########    ##  ##    ##  ##  ##   ##          
+                   ###    ####  ##    ##   ##  ##  ##   ##
+                   ###    ####  ##    ##   ##  ##  ##   #######         
+                   ##########    ##  ##    ##  ##  ##   ##   
+                   #########      ####     ##  ######   ########      
 
-###############################################################
-###############################################################
+          ###############################################################
+          ###############################################################
   
 @app.errorhandler(404)
 def not_found_error(error):
@@ -555,7 +469,7 @@ if not app.debug:
     app.logger.info('errors')
 
 #----------------------------------------------------------------------------#
-# Launch.
+#            ****************      Launch.    **********************         #
 #----------------------------------------------------------------------------#
 if __name__ == '__main__':
     app.run(debug=True)
